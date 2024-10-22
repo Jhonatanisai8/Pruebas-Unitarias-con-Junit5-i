8ce40c3185e434697adcf4ab7e65840cdd30905f
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-10-21 23:41:52 -0500

    metodo testSaldoCuenta

--- src/test/java/com/isai/junit5app/ejemplos/models/CuentaTest.java
+++ src/test/java/com/isai/junit5app/ejemplos/models/CuentaTest.java
@@ -9,12 +9,22 @@ public class CuentaTest {
     //metodo a testear
     @Test
     void testNombreCuenta() {
-        Cuenta cuenta = new Cuenta("honatan", new BigDecimal("1000.12345"));
+        Cuenta cuenta = new Cuenta("Jhonatan", new BigDecimal("1000.12345"));
         String esperado = "Jhonatan";
         String real = cuenta.getPersona();
-        
+
         assertEquals(esperado, real);
         assertTrue(real.equals("Jhonatan"));
     }
-    
+
+    @Test
+    void testSaldoCuenta() {
+
+        Cuenta cuenta = new Cuenta("Jhonatan", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+        
+    }
 }
